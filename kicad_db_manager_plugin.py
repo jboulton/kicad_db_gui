9f68cbd8f4e004ad2551e6abf7da3ae83384d331
James Boulton <james@dashio.io>
2026-09-23 09:48:37 +1200

    update doc

--- kicad_db_manager_plugin.py
+++ kicad_db_manager_plugin.py
@@ -1,14 +1,22 @@
 # --- Install ---
-# 1. Symlink the launcher onto your PATH:
+# 1. Set up the Python virtual environment for the DB manager app:
+#      cd ~/git/kicad_db_gui
+#      python3 -m venv venv
+#      source venv/bin/activate
+#      pip install -r requirements.txt
+#      deactivate
+#    (run.sh activates this venv automatically each time it's launched)
+#
+# 2. Symlink the launcher onto your PATH:
 #      ln -s ~/git/kicad_db_gui/run.sh /opt/homebrew/bin/kicad-db
 #      (Intel Macs: use /usr/local/bin/kicad-db instead)
 #
-# 2. Copy this plugin + icons into KiCad's plugin folder:
-#      mkdir -p ~/Documents/KiCad/10.0/scripting/plugins/icons
-#      cp kicad_db_manager_plugin.py ~/Documents/KiCad/9.0/scripting/plugins/
-#      cp icon.png icon@2x.png ~/Documents/KiCad/9.0/scripting/plugins/icons/
+# 3. Copy this plugin + icons into KiCad's plugin folder:
+#      mkdir -p ~/Documents/KiCad/9.0/scripting/plugins/icons
+#      cp kicad_db_manager_plugin.py ~/Documents/KiCad/10.0/scripting/plugins/
+#      cp icon.png icon@2x.png ~/Documents/KiCad/10.0/scripting/plugins/icons/
 #
-# 3. In PCBNew: Tools -> External Plugins -> Refresh Plugins
+# 4. In PCBNew: Tools -> External Plugins -> Refresh Plugins
 #    (or restart KiCad)
 
 
